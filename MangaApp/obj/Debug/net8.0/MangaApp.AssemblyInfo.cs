054e44c4f0fbbcc9e73cef7db4106c06f6c37cbd
RKPH <pnghung2003@gmail.com>
2024-07-27 10:41:14 +0700

    add validation to delete user

--- MangaApp/obj/Debug/net8.0/MangaApp.AssemblyInfo.cs
+++ MangaApp/obj/Debug/net8.0/MangaApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MangaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29449f3eedb2049374c1211b6daf7e692a0003a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7bcc30fa5527bfb9e6b585f54ba79fcb5b351a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MangaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MangaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
